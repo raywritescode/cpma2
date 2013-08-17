b4e3842f5b9dc2082bc43cba575463e45b84d73b
raywritescode <raywritescode@server.fake>
2013-08-16 19:44:07 -0700

    Adding Chapter 2, Programming Project 1

---
+++ ch02/c2p1.c
@@ -0,0 +1,33 @@
+/* c2p1.c
+
+   Ray Santos
+   August 16, 2013
+
+   C Programming: A Modern Approach, Second Edition
+   Chapter 2. Programming Project 1
+
+   Write a program that uses printf to display the
+   following picture on the screen:
+
+          *
+         *
+        *
+   *   *
+    * *
+     *
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("        *\n");
+    printf("       *\n");
+    printf("      *\n");
+    printf(" *   *\n");
+    printf("  * *\n");
+    printf("   *\n");
+
+    return 0;
+}
