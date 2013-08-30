40e214b7b231c4797dc14145acb2cdf6a0bc4c54
raywritescode <raywritescode@server.fake>
2013-08-29 21:11:05 -0700

    Updated Chapter 6, Programming Project 4 (to use while loop)

--- ch06/c6p04.c
+++ ch06/c6p04.c
@@ -33,33 +33,28 @@ int main(void)
     printf("Enter value of trade (0 to exit): ");
     scanf("%f", &value);
 
-    if (value == 0)
-       return 0;
-    else {
-        do {
-            if (value < 2500.00f)
-                commission = 30.00f + .017f * value;
-            else if (value < 6250.00f)
-                commission = 56.00f + .0066f * value;
-            else if (value < 20000.00f)
-                commission = 76.00f + .0034f * value;
-            else if (value < 50000.00f)
-                commission = 100.00f + .0022f * value;
-            else if (value < 500000.00f)
-                commission = 155.00f + .0011f * value;
-            else
-                commission = 255.00f + .0009f * value;
+    while (value != 0) {
+        if (value < 2500.00f)
+            commission = 30.00f + .017f * value;
+        else if (value < 6250.00f)
+            commission = 56.00f + .0066f * value;
+        else if (value < 20000.00f)
+            commission = 76.00f + .0034f * value;
+        else if (value < 50000.00f)
+            commission = 100.00f + .0022f * value;
+        else if (value < 500000.00f)
+            commission = 155.00f + .0011f * value;
+        else
+            commission = 255.00f + .0009f * value;
 
-            if (commission < 39.00f)
-                commission = 39.00f;
+        if (commission < 39.00f)
+            commission = 39.00f;
 
-            printf("Commission: $%.2f\n", commission);
+        printf("Commission: $%.2f\n", commission);
 
-            printf("\nEnter value of trade (0 to exit): ");
-            scanf("%f", &value);
-        
-        } while (value != 0);
-    }
+        printf("\nEnter value of trade (0 to exit): ");
+        scanf("%f", &value);
+    } 
 
     return 0;
 }
