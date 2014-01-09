819554ecf3d553208e83b07701e9bb90320a06f4
raywritescode <raywritescode@server.fake>
2014-01-09 15:42:57 -0800

    Chapter Example: Reverses a series of numbers using a variable-length array

---
+++ ch08/reverse2.c
@@ -0,0 +1,31 @@
+/* reverse2.c
+ *
+ * Reverses a series of numbers using a variable-length
+ * array - C99 only.
+ * 
+ * Page 174
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+   int i, n;
+
+   printf("\nHow many numbers do you want to reverse? ");
+   scanf("%d", &n);
+
+   int a[n];   /* C99 only -- length of array depends on n */
+
+   printf("Enter %d numbers: ", n);
+   for (i = 0; i < n; i++)
+      scanf("%d", &a[i]);
+
+   printf("In reverse order:");
+   for (i = n - 1; i >= 0; i--)
+      printf(" %d", a[i]);
+   printf("\n");
+
+   return 0;
+}
