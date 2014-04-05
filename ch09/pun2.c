e6c3eb13845ac5fcdce256baee41fcbe049ea2a8
raywritescode <raywritescode@users.noreply.github.com>
2014-04-05 15:27:22 -0700

    pun2.c - Prints a bad pun.

---
+++ ch09/pun2.c
@@ -0,0 +1,18 @@
+/* pun2.c
+ *
+ * Prints a bad pun
+ *
+ */
+
+#include <stdio.h>
+
+void print_pun(void)
+{
+   printf("To C, or not to C: that is the question.\n");
+}
+
+int main(void)
+{
+   print_pun();
+   return 0;
+}
