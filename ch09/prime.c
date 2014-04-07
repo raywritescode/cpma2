083892568b7bdb97a282daa31d43bbde27e411b0
raywritescode <raywritescode@users.noreply.github.com>
2014-04-07 13:26:16 -0700

    prime.c - Tests whether a number is prime.

---
+++ ch09/prime.c
@@ -0,0 +1,35 @@
+/* prime.c
+ *
+ * Tests whether a number is prime.
+ *
+ */
+
+#include <stdio.h>
+#include <stdbool.h> /* C99 only */
+
+bool is_prime(int n)
+{
+   int divisor;
+
+   if (n <= 1)
+      return false;
+   for (divisor = 2; divisor * divisor <=n; divisor++)
+      if (n % divisor == 0)
+         return false;
+   return true;
+}
+
+int main(void)
+{
+   int n;
+  
+   printf("Enter a number: ");
+   scanf("%d", &n);
+
+   if (is_prime(n))
+      printf("Prime\n");
+   else
+      printf("Not prime\n");
+
+   return 0;
+}
