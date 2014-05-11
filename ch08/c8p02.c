8e958eaced1a8ca9d757469956b55dc0bab32f3b
raywritescode <raywritescode@users.noreply.github.com>
2014-05-10 19:19:47 -0700

    Added Chapter 8, Programming Project 2

--- ch08/c8p02.c
+++ ch08/c8p02.c
@@ -12,24 +12,40 @@
  *    Occurrences:  1  2  2  0  0  1  0  1  0  1
  *
  * Ray Santos
- * May 9, 2014
+ * May 10, 2014
  *
  */
 
-#include <stdbool.h>  /* C99 only */
 #include <stdio.h>
+
+#define DIGITS 10
 
 int main(void)
 {
-   bool digit_seen[10] = {false};
-   int to_print[10] = {0};
-   int digit, isEmpty, i;
+   int digit_count[DIGITS] = {0};
+   int digit, i;
    long n;
 
    printf("\nEnter a number: ");
    scanf("%ld", &n);
 
-   / * In progress */ 
- 
+   /* Count number of times each digit seen */
+   while (n > 0) {
+      digit = n % 10;                  // gets the right-most digit.
+      digit_count[digit]++;            // add 1 to index matching the right-most digit.
+      n /= 10;                         // remove the right-most digit from the number.
+   } 
+
+   /* Print the Digit header */
+   printf("Digit:      ");
+   for (i = 0; i < DIGITS; i++)
+      printf("%3d", i);
+
+   /* Print Occurrences */
+   printf("\nOccurrences:");
+   for (i = 0; i < DIGITS; i++)
+      printf("%3d", digit_count[i]);
+   printf("\n");
+
    return 0;
 }
