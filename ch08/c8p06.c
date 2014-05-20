bbd0aa3cc0cc84605288b87cf7f9b18b4e113b11
raywritescode <raywritescode@users.noreply.github.com>
2014-05-19 17:40:37 -0700

    Final version - Chapter 8, Programming Project 6

--- ch08/c8p06.c
+++ ch08/c8p06.c
@@ -22,24 +22,51 @@
  * through the array, translating and printing characters one by one.
  *
  * Ray Santos
- * May 18, 2014
+ * May 19, 2014
  *
  */
 
 #include <stdio.h>
 
+#define MSG_SIZE 100
+
 int main(void)
 {
-   char message[];
+   char message[MSG_SIZE] = {0};
+   char ch;
    int i;
 
-   printf("Enter message: ");
-   /* TODO: read in an array of characters */
+   printf("Enter a message: ");
    
-   /* TODO: translate to B1FF-speak */
+   i = 0;
+   while (( ch = getchar()) != '\n') {
+      message[i] = toupper(ch);
+      i++;
+   }
+   message[i] = '\0';
 
-   printf("In B1FF-speak: ");
-   /* TODO: output the translated characters */
-   
+   printf("In B1FF-speak:   ");
+
+   i = 0;
+   while (message[i] != '\n') {
+      switch (message[i]) {
+         case 'A': message[i] = '4';
+                   break;
+         case 'B': message[i] = '8';
+                   break;
+         case 'E': message[i] = '3';
+                   break;
+         case 'I': message[i] = '1';
+                   break;
+         case 'O': message[i] = '0';
+                   break;
+         case 'S': message[i] = '5';
+                   break;
+   }
+      putchar(message[i]);
+      i++;
+   }
+   printf("!!!!!\n"); 
+ 
    return 0;
 }
