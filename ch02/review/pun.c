53551d90304b0e53d39cc89bd4ab0d3fa98d6e4e
raywritescode <raywritescode@users.noreply.github.com>
2015-06-17 20:28:39 -0700

    Added - ch02/review/pun.c

---
+++ ch02/review/pun.c
@@ -0,0 +1,17 @@
+/* pun.c
+
+   C programming: A Modern Approach, Second Edition
+   Chapter 2. Page 10
+
+   Ray Santos
+   June 17, 2015
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("To C, or not to C: that is the question.\n");
+  return 0; 
+}
