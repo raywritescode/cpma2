7531f10d64cc94c5a06f9d8fa9751d04eb65c8af
raywritescode <raywritescode@users.noreply.github.com>
2015-06-21 15:08:56 -0700

    Added - ch02/exercises/ch02ex07.c

---
+++ ch02/exercises/ch02ex07.c
@@ -0,0 +1,23 @@
+/* ch02ex07.c
+
+  C programming: A Modern Approach, Second Edition
+  Chapter 2. Page 33. Exercise 7
+
+  Ray Santos
+  June 21, 2015
+
+  Which of the following are keywords in C?
+
+  a) for
+  b) If
+  c) main
+  d) printf
+  e) while
+
+  a) a keyword in C
+  b) not a keyword in C. keywords are lowercase. if is a keyword in C.
+  c) not a keyword in C. main is function name.
+  d) not a keyword in C. printf is a function name.
+  e) a keyword in C
+
+*/
