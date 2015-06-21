656771fdfe3446c8a3b64ac801230ddc1f902fe6
raywritescode <raywritescode@users.noreply.github.com>
2015-06-21 13:52:41 -0700

    Added - ch02/exercises/ch02ex05.c

---
+++ ch02/exercises/ch02ex05.c
@@ -0,0 +1,21 @@
+/* ch02ex05.txt
+
+  C programming: A Modern Approach, Second Edition
+  Chapter 2. Page 33. Exercise 5
+
+  Ray Santos
+  June 21, 2015
+
+  Which of the following are not legal C identifiers?
+
+  a) 100_bottles
+  b) _100_bottles
+  c) one__hundred__bottles
+  d) bottles_by_the_hundred
+
+  a) is not a legal C identifier. Identifiers may not begin with a number.
+  b) is a legal C identifier. Identifiers must begin with a letter or underscore.
+  c) is a legal C identifier. Identifiers must begin with a letter or underscore.
+  d) is a legal C identifier. Identifiers must begin with a letter or underscore.
+
+*/
