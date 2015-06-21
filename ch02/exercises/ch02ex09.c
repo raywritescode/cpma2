789d52835b0e953522332b0a54c7a5e337b89a0c
raywritescode <raywritescode@users.noreply.github.com>
2015-06-21 15:17:48 -0700

    Added - ch02/exercises/ch02ex09.c

---
+++ ch02/exercises/ch02ex09.c
@@ -0,0 +1,18 @@
+/* ch02ex09.c
+
+  C programming: A Modern Approach, Second Edition
+  Chapter 2. Page 34. Exercise 9
+
+  Ray Santos
+  June 21, 2015
+
+  Insert spaces between the tokens in Exercise 8 to
+  make the statement easier to read.
+
+  Original statement:
+    answer=(3*q-p*p)/3;
+
+  Statement with spaces:
+    answer = (3 * q - p * p) / 3;
+
+*/
