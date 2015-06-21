6167364e52a2154d6bad8a32d2cdb510bdba9f07
raywritescode <raywritescode@users.noreply.github.com>
2015-06-21 15:14:29 -0700

    Added - ch02/exercises/ch02ex08.c

---
+++ ch02/exercises/ch02ex08.c
@@ -0,0 +1,29 @@
+/* ch02ex08.c
+
+  C programming: A Modern Approach, Second Edition
+  Chapter 2. Page 34. Exercise 8
+
+  Ray Santos
+  June 21, 2015
+
+  Q: How many tokens are thre in the following statement?
+
+     answer=(3*q-p*p)/3;
+
+  A: 14. They are:
+       1) answer
+       2) =
+       3) (
+       4) 3
+       5) *
+       6) q
+       7) -
+       8) p
+       9) *
+      10) p
+      11) )
+      12) /
+      13) 3
+      14) ;
+
+*/
