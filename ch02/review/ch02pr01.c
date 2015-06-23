369943456dbb599e12b928d30f19e3cbfcf07255
raywritescode <raywritescode@users.noreply.github.com>
2015-06-22 22:02:12 -0700

    Added - ch02/review/ch02pr01.c

---
+++ ch02/review/ch02pr01.c
@@ -0,0 +1,36 @@
+/* ch02pr01.c
+
+   C programming: A Modern Approach, Second Edition
+   Chapter 2. Page 34. Programming Project 01
+
+   Ray Santos
+   June 22, 2015
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("       *\n");
+  printf("      *\n");
+  printf("     *\n");
+  printf("*   *\n");
+  printf(" * *\n");
+  printf("  *\n");
+
+  return 0;
+}
+
+/* Test run
+
+$ gcc ch02pr01.c 
+$ ./a.out
+       *
+      *
+     *
+*   *
+ * *
+  *
+
+*/
