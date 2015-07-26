aeb6ce107c328a525aaa49dc9fa1ecf6e82e4035
raywritescode <raywritescode@users.noreply.github.com>
2015-07-25 20:49:07 -0700

    Added - ch03/exercises/ch03ex03.c

---
+++ ch03/exercises/ch03ex03.c
@@ -0,0 +1,73 @@
+/* ch03ex03.c
+
+  C programming: A Modern Approach, Second Edition
+  Chapter 3. Page 49. Exercise 3
+
+  Ray Santos
+  July 25, 2015
+
+  For each of the following pairs of scanf format strings, indicate whether
+  or not the two strings are equivalent. If they're not, show how they can
+  be distinguished.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int x = 1;
+  int y = 2;
+  int z = 3;
+
+  float a = 10.0;
+  float b = 20.0;
+  float c = 30.0;
+
+  // (a) "%d" versus " %d"
+  printf("(a) \"%%d\" versus \" %%d\"\n\n");
+  printf("\t \"%%d\" = %d\n", x);
+  printf("\t\" %%d\" =  %d\n\n", x); 
+
+  // (b) "%d-%d-%d" versus "%d -%d -%d"
+  printf("(b) \"%%d-%%-d-%%d\" versus \"%%d -%%d -%%d\"\n\n");
+  printf("\t   \"%%d-%%d-%%d\" = %d-%d-%d\n", x, y, z);
+  printf("\t\"%%d -%%d - %%d\" = %d -%d -%d\n\n", x, y, z);
+
+  // (c) "%f" versus "%f "
+  printf("(c) \"%%f\" versus \"%%f \"\n\n");
+  printf("\t \"%%f\" = %f\n", a);
+  printf("\t\"%%f \" = %f \n\n", a);
+
+  // (d) "%f,%f" versus "%f, %f"
+  printf("(d) \"%%f,%%f\" versus \"%%f, %%f\"\n\n");
+  printf("\t \"%%f,%%f\" = %f,%f\n", a, b);
+  printf("\t\"%%f, %%f\" = %f, %f\n\n", a, b);
+
+  return 0;
+}
+
+/* Program Execution
+
+$ gcc ch03ex03.c 
+$ ./a.out
+(a) "%d" versus " %d"
+
+	 "%d" = 1
+	" %d" =  1
+
+(b) "%d-%-d-%d" versus "%d -%d -%d"
+
+	   "%d-%d-%d" = 1-2-3
+	"%d -%d - %d" = 1 -2 -3
+
+(c) "%f" versus "%f "
+
+	 "%f" = 10.000000
+	"%f " = 10.000000 
+
+(d) "%f,%f" versus "%f, %f"
+
+	 "%f,%f" = 10.000000,20.000000
+	"%f, %f" = 10.000000, 20.000000
+
+*/
