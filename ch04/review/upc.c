129caa7a7194dd852ac3eaf74a66a8635e02dab0
raywritescode <raywritescode@users.noreply.github.com>
2015-09-01 21:53:08 -0700

    Added - ch04/review/upc.c

---
+++ ch04/review/upc.c
@@ -0,0 +1,36 @@
+/* upc.c
+
+  Book example: Computes a Universal Product Code check digit
+
+  C Programming: A Modern Approach, Second Edition
+  Chapter 4, Page 57
+
+  Ray Santos
+  September 1, 2015
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5;
+  int first_sum, second_sum, total;
+
+  printf("Enter the first (single) digit: ");
+  scanf("%1d", &d);
+
+  printf("Enter first group of five digits: ");
+  scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
+
+  printf("Enter second group of five digits: ");
+  scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
+
+  first_sum = d + i2 + i4 + j1 + j3 + j5;
+  second_sum = i1 + i3 + i5 + j2 + j4;
+  total = 3 * first_sum + second_sum;
+
+  printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+
+  return 0;
+}
