a55da48f90a670097b896a0ca4e1a8e5401cfe9d
raywritescode <raywritescode@users.noreply.github.com>
2015-11-29 18:33:10 -0800

    Added - ch05/review/broker.c

---
+++ ch05/review/broker.c
@@ -0,0 +1,56 @@
+/* broker.c
+
+   Book example: Calculates a broker's commission
+
+   C Programming: A Modern Approach, Second Edition
+   Chapter 5, Page 81
+
+   Ray Santos
+   November 29, 2015
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+   float commission, value;
+
+   printf("Enter value of trade: ");
+   scanf("%f", &value);
+
+   if (value < 2500.00f)
+      commission = 30.00f + .017f * value;
+   else if (value < 6250.00f)
+      commission = 56.00f + .0066f * value;
+   else if (value < 20000.00f)
+      commission = 76.00f + .0034f * value;
+   else if (value < 50000.00f)
+      commission = 100.00f + .0022f * value;
+   else if (value < 500000.00f)
+      commission = 155.00f + .0011f * value;
+   else
+      commission = 255.00f + .0009f * value;
+
+   if (commission < 39.00f)
+      commission = 39.00f;
+
+   printf("Commission: $%.2f\n", commission);
+
+   return 0;
+}
+
+/* Example runs
+
+$ gcc -Wall broker.c 
+$ ./a.out
+Enter value of trade: 2499
+Commission: $72.48
+$ ./a.out
+Enter value of trade: 30000
+Commission: $166.00
+$ ./a.out
+Enter value of trade: 3
+Commission: $39.00
+
+*/
