71e6650367a1e49dae5444efcfea8846ca80deba
raywritescode <raywritescode@users.noreply.github.com>
2015-12-10 21:54:24 -0800

    Added Chapter 8, Programming Project 9 - problem statement and initial code.

---
+++ ch08/c8p09.c
@@ -0,0 +1,61 @@
+/* c8p09.c
+
+  C Programming: A Modern Approach, Second Edition
+  Chapter 8, Programming Project 9, Page 179
+
+  Write a program that generates a "random walk" across a 10x10 array.
+  The array will contain characters (all '.' initially). The program
+  must randomly "walk" from element to element, always going up, down,
+  left, or right by one element. The elements visisted by the program
+  will be labeled with the letters A through Z, in the order visisted.
+  Here's an example of the desired output:
+
+    A . . . . . . . . .
+    B C D . . . . . . .
+    . F E . . . . . . .
+    H G . . . . . . . .
+    I . . . . . . . . .
+    J . . . . . . . Z .
+    K . . R S T U V Y .
+    L M P Q . . . W X .
+    . N O . . . . . . .
+    . . . . . . . . . .
+
+  Hint: Use the srand and rand functions (see deal.c) to generate random
+  numbers. After generating a number, look at its remainder when divided
+  by 4. There are four possible values for the remainder (0, 1, 2, and 3)
+  indicating the direction of the next move. Before performing a move,
+  check that (a) it won't go outside the array, and (b) it doesn't take 
+  us to an element that already has a letter assigned. If either condition
+  is violated, try moving in another direction. If all four directions are
+  blocked, the program must terminate. Here's an example of a premature
+  termination:
+
+    A B G H I . . . . .
+    . C F . J K . . . .
+    . D E . M L . . . .
+    . . . . N O . . . .
+    . . W X Y P Q . . .
+    . . V U T S R . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+    . . . . . . . . . .
+
+  Y is blocked on all four sides, so there's no place to put Z.
+
+  Ray Santos
+  December 10, 2015
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+
+
+   return 0;
+}
