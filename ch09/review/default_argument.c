3d48876e9727057211d695c83d28618785921497
raywritescode <raywritescode@users.noreply.github.com>
2015-12-19 23:00:26 -0800

    Added - ch09/review/default_argument.c

---
+++ ch09/review/default_argument.c
@@ -0,0 +1,37 @@
+/* default_argument.c
+ 
+   Book example showing why relying on the default
+   argument promotions is dangerous.
+ 
+   C Programming: A Modern Approach, Second Edition
+   Chapter 9, Page 195
+
+   Ray Santos
+   December 19, 2015
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+   double x = 3.0;
+
+   printf("Square: %d\n", square(x));  // compiler doesn't know square
+                                       // expects an int. Defaults to double.
+   return 0;
+}
+
+int square(int n)
+{
+   return n * n;
+}
+
+/* Example of the C compiler error
+
+$ gcc -Wall default_argument.c 
+default_argument.c: In function ‘main’:
+default_argument.c:20:4: warning: implicit declaration of function ‘square’ [-Wimplicit-function-declaration]
+$ 
+
+*/
